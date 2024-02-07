605f58ca45bc8569b66ec37dae9b4648ead54e1a
vamsidharpanithi <145138793+vamsidharpanithi@users.noreply.github.com>
2024-02-07 16:55:32 -0500

    Update WebApplicationTests.java

--- src/test/java/com/CreateAPI/WebApplication/WebApplicationTests.java
+++ src/test/java/com/CreateAPI/WebApplication/WebApplicationTests.java
@@ -1,13 +1,31 @@
 package com.CreateAPI.WebApplication;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class WebApplicationTests {
+    @LocalServerPort
+    private int port;
+    @Autowired
+    private TestRestTemplate testRestTemplate;
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void testHealthz() {
+        String url = "http://localhost:"+port+"/healthz";
+        ResponseEntity<String> responseEntity = testRestTemplate.getForEntity(url,String.class);
+        assertEquals(HttpStatus.OK,responseEntity.getStatusCode());
+    }
+
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
