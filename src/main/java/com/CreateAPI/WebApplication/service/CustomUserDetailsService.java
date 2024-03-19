ab8e69d760b854e51e4d8d2747582b09e879faf0
Vamsidhar858 <145138793+Vamsidhar858@users.noreply.github.com>
2024-03-19 13:11:57 -0400

    updated packer and logs

--- src/main/java/com/CreateAPI/WebApplication/service/CustomUserDetailsService.java
+++ src/main/java/com/CreateAPI/WebApplication/service/CustomUserDetailsService.java
@@ -1,6 +1,8 @@
 package com.CreateAPI.WebApplication.service;
 
 import com.CreateAPI.WebApplication.repository.UserRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,10 +17,12 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
 
+    private static final Logger logger = LogManager.getLogger(CustomUserDetailsService.class);
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username);
         if(user==null){
+            logger.error("No User found with username "+username);
             throw new UsernameNotFoundException("No User found with username "+username);
         }
         return new org.springframework.security.core.userdetails.User(user.getUsername(),user.getPassword(), Collections.emptyList());
